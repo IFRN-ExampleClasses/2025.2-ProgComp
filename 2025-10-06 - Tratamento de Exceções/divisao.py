35deeb24528cac9790d354dfd6d6d541362e550c
charles-freitas <ccmfreitas@gmail.com>
2025-10-06 19:39:21 -0300

    Tratamento de Exceções

---
+++ 2025-10-06 - Tratamento de Exceções/divisao.py
@@ -0,0 +1,24 @@
+'''
+	Você deve criar um programa que calcule o quociente entre dois números 
+   inteiros fornecidos pelo usuário. Durante o processo, é necessário 
+   tratar as seguintes exceções:
+
+		- Caso o segundo número seja zero, o programa deve exibir uma mensagem 
+		  informando que a divisão por zero não é permitida.
+
+		- Caso o usuário insira um valor que não seja um número inteiro, o 
+        programa deve exibir uma mensagem solicitando que o usuário insira 
+        apenas números inteiros.
+
+	Instruções:
+
+		- Solicite ao usuário para inserir o primeiro número inteiro.
+
+		- Solicite ao usuário para inserir o segundo número inteiro.
+
+		- Calcule o quociente entre os dois números, tratando as exceções descritas acima.
+'''
+
+intDividendo = int(input('Informe o Dividendo: '))
+intDivisor = int(input('Informe o Divisor: '))
+
