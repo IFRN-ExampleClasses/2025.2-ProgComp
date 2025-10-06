1885a7190e2cf85c6557c8dc4dad6c6f89172dbe
charles-freitas <ccmfreitas@gmail.com>
2025-10-06 20:43:44 -0300

    Tratamento de Exceções

--- 2025-10-06 - Tratamento de Exceções/divisao.py
+++ 2025-10-06 - Tratamento de Exceções/divisao.py
@@ -16,9 +16,20 @@
 
 		- Solicite ao usuário para inserir o segundo número inteiro.
 
-		- Calcule o quociente entre os dois números, tratando as exceções descritas acima.
+		- Calcule o quociente entre os dois números, tratando as exceções 
+        descritas acima.
 '''
+import sys
 
-intDividendo = int(input('Informe o Dividendo: '))
-intDivisor = int(input('Informe o Divisor: '))
-
+try:
+   intDividendo = int(input('Informe o Dividendo: '))
+   intDivisor   = int(input('Informe o Divisor..: '))
+   quociente    = intDividendo / intDivisor
+except ZeroDivisionError:
+   sys.exit('ERRO: Divisão por Zero não é permitida...')
+except ValueError:
+   sys.exit('ERRO: Informe apenas números inteiros...')
+except Exception as strErro:
+   sys.exit(f'EXCEÇÃO: {sys.exc_info()[0]} -> {strErro}')
+else:
+   print(f'{intDividendo} / {intDivisor} = {quociente:.2f}')
