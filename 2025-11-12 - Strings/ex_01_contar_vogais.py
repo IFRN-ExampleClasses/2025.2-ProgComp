09a3695d17506f4161bf30bd34e127120db1daa9
charles-freitas <ccmfreitas@gmail.com>
2025-11-12 20:46:57 -0300

    Strings

---
+++ 2025-11-12 - Strings/ex_01_contar_vogais.py
@@ -0,0 +1,4 @@
+'''
+   Exemplo 01: Programa para contar quantas vogais existem em uma string 
+   fornecida pelo usuário.
+'''
---
+++ 2025-11-12 - Strings/ex_02_contar_palavras.py
@@ -0,0 +1,4 @@
+'''
+   Exemplo 02: Programa para contar quantas palavras existem em uma frase
+   fornecida pelo usuário.
+'''
